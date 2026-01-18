a0421ad05c94028f8143cc41576fc8906949a65d
ivanbap <ivanbodemberg@gmail.com>
2026-01-17 23:58:21 -0300

    att

--- script.js
+++ script.js
@@ -12,17 +12,20 @@ faqItems.forEach(item => {
 
 $(document).ready(function() {
     // Abre e fecha o menu ao clicar no botão
-    $('.btn-mobile').on('click', function() {
+    $('.btn-mobile').on('click', function(e) {
+        e.preventDefault();
         $('.mobile-menu').toggleClass('active');
         
-        // Troca o ícone de barras pelo de fechar (X)
+        // Troca o ícone
         const icon = $(this).find('i');
         icon.toggleClass('fa-bars fa-xmark');
     });
 
-    // Fecha o menu automaticamente ao clicar em qualquer link
+    // Fecha o menu automaticamente ao clicar em qualquer link (âncora)
     $('.mobile-list a').on('click', function() {
         $('.mobile-menu').removeClass('active');
+        
+        // Garante que o ícone volte a ser o "hambúrguer"
         $('.btn-mobile i').addClass('fa-bars').removeClass('fa-xmark');
     });
 });
